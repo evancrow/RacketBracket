8fde18d68b047e99ccb36e3f66427dc9f843d6a9
Evan Crow <evanwcrow@gmail.com>
2022-05-21 10:03:25 -0700

    Fix tests

--- Racket BracketUITests/Racket_BracketUITests.swift
+++ Racket BracketUITests/Racket_BracketUITests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class Racket_BracketUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -20,22 +19,5 @@ class Racket_BracketUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
